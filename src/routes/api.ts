8bf0fc7eae62e18f79b30d8a9557e72a6ad2262a
AlfitoDimasss <dimasalfito89@gmail.com>
2025-07-12 15:31:16 +0700

    add password encryption and add auth api route

--- src/routes/api.ts
+++ src/routes/api.ts
@@ -6,5 +6,6 @@ const router = express.Router();
 
 router.get("/dummy", dummyController.dummy);
 router.post("/auth/register", authController.register);
+router.post("/auth/login", authController.login);
 
 export default router;
